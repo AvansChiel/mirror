fe0cc3e4b997a3cbe6fed11973ec13d5cfb442b6
AvansChiel <cgmarts@avans.nl>
2021-01-11 20:22:53 +0100

    dir

--- client/client.cpp
+++ client/client.cpp
@@ -11,35 +11,45 @@
 #include <asio.hpp>
 
 int main() {
-    try {
-        const char* server_address{ "localhost" };
-        const char* server_port{ "12345" };
-        const char* prompt{ "avansync> " };
-        const char* lf{ "\n" };
-        const char* crlf{ "\r\n" };
+	try {
+		const char* server_address{ "localhost" };
+		const char* server_port{ "12345" };
+		const char* prompt{ "avansync> " };
+		const char* lf{ "\n" };
+		const char* crlf{ "\r\n" };
+		const std::string rootPath = "E:/datamirror/client";
+		bool finished = false;
+		asio::ip::tcp::iostream server{ server_address, server_port };
+		if (!server) throw std::runtime_error("could not connect to server");
 
-        asio::ip::tcp::iostream server{ server_address, server_port };
-        if (!server) throw std::runtime_error("could not connect to server");
+		while (server) {
+			std::string resp;
+			while (!finished) {
+				if (getline(server, resp)) {
+					if (resp.substr(resp.length() - 1, 1) == "\r" ) {
+						resp.erase(resp.end() - 1); // remove '\r'
+						finished = true;
+					}
+					
+					std::cout << resp << lf;
+					if (resp == "Bye.") break;
 
-        while (server) {
-            std::string resp;
-            if (getline(server, resp)) {
-                resp.erase(resp.end() - 1); // remove '\r'
-                std::cout << resp << lf;
-                if (resp == "Bye.") break;
+				}
+				
+			}
 
-                std::cout << prompt;
-                std::string req;
-                if (getline(std::cin, req)) {
-                    server << req << crlf;
-                }
-            }
-        }
+			std::cout << prompt;
+			std::string req;
+			if (getline(std::cin, req)) {
+				finished = false;
+				server << req << crlf;
+			}
+		}
 
-    }
-    catch (const std::exception& ex) {
-        std::cerr << "client: " << ex.what() << '\n';
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
+	}
+	catch (const std::exception& ex) {
+		std::cerr << "client: " << ex.what() << '\n';
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -9,12 +9,84 @@
 #include <string>
 #include <stdexcept>
 #include <asio.hpp>
+#include <filesystem>
+
+const std::string rootPath = "E:/datamirror/server";
+const int server_port{ 12345 };
+const char* lf{ "\n" };
+const char* crlf{ "\r\n" };
+
+template<typename T>
+std::string toString(const T& t) {
+    std::ostringstream oss;
+    oss << t;
+    return oss.str();
+}
+
+template<typename T>
+T fromString(const std::string& s) {
+    std::istringstream stream(s);
+    T t;
+    stream >> t;
+    return t;
+}
+
+template <typename TP>
+std::time_t to_time_t(TP tp)
+{
+    using namespace std::chrono;
+    auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now()
+        + system_clock::now());
+    return system_clock::to_time_t(sctp);
+}
+
+std::string dir(){
+    using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
+    std::string resultString = "";
+    for (const auto& dirEntry : recursive_directory_iterator(rootPath)) {
+        std::string type;
+
+        if (dirEntry.is_directory()) {
+            resultString += "D|";
+        }
+        else {
+            resultString += "F|";
+        }
+        char buffer[32];
+        // Format: Mo, 15.06.2009 20:20:00
+        struct tm newTime;
+        std::time_t timestamp = to_time_t<decltype(dirEntry.last_write_time())>(dirEntry.last_write_time());
+        localtime_s(&newTime, &timestamp);
+        
+        resultString += dirEntry.path().filename().string() + "|";
+        std::string day = std::to_string(newTime.tm_mday);
+        std::string month = std::to_string(newTime.tm_mon + 1);
+        std::string year = std::to_string(newTime.tm_year + 1900);
+
+        std::string hour = std::to_string(newTime.tm_hour);
+        std::string min = std::to_string(newTime.tm_min);
+        std::string sec = std::to_string(newTime.tm_sec);
+        resultString += day + "-" + month + "-" + year + " " + hour + ":" + min + ":" + sec + "|";
+
+        std::string filesize;
+        if (dirEntry.file_size() < 0) {
+            filesize = "0";
+        }
+        else {
+            filesize = std::to_string(dirEntry.file_size());
+        }
+        resultString += filesize + "";
+        resultString += lf;
+    }
+    //remove last newline
+    resultString.erase(resultString.end() - 1);
+    return resultString;
+}
 
 int main() {
     try {
-        const int server_port{ 12345 };
-        const char* lf{ "\n" };
-        const char* crlf{ "\r\n" };
+
+
 
         asio::io_context io_context;
         asio::ip::tcp::acceptor server{ io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), server_port) };
@@ -35,11 +107,18 @@ int main() {
                     client << "Bye." << crlf;
                     std::cerr << "will disconnect from client " << client.socket().local_endpoint() << lf;
                     break;
+                }else if (request == "info") {
+                    client << "Data Mirror server 1.0, copyright (c) 2021 Chiel Arts." << crlf;
+                }
+                else if (request == "dir") {
+                    client << dir() << crlf;
                 }
                 else {
                     client << request << crlf; // simply echo the request
                 }
             }
+            
+
         }
 
     }
